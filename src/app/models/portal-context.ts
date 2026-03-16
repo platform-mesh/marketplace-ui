030582eb3519fc39f5941d9dba68985130e94c83
Grzegorz Krajniak <gkrajniak@gmail.com>
2026-03-16 11:56:02 +0100

    cleanup implementation (#39)
    Co-authored-by: Grzegorz Krajniak <grzegorz.krajniak@sap.com>

--- src/app/models/portal-context.ts
+++ src/app/models/portal-context.ts
@@ -1,7 +1,5 @@
-// todo gkr remove what's not needed env ???
 export interface PortalContext extends Record<string, string> {
   crdGatewayApiUrl: string;
   accountsServiceApiUrl: string;
-  automaticDGraphqlApiUrl: string;
   environment: 'dev' | 'int' | 'prod';
 }
