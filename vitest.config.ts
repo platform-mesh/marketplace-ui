8dea174621f17465cad620ba13d7e61a3abeff67
Sobyt483 <kravczov.andrian@mail.ru>
2026-03-19 01:29:18 +0400

    feat: adjust configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'jsdom',
     coverage: {
       enabled: true,
       provider: 'v8',
-      thresholds: { branches: 70, functions: 75 },
+      thresholds: { statements: 71, branches: 75, functions: 78, lines: 73 },
       exclude: ['**/*.po.ts'],
     },
     server: {
